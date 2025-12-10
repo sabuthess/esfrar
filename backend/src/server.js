c5052c81a74cb25fbc926d3d67cc7f936547b774
sabuthess <dilan.developer22@gmail.com>
2025-12-09 19:48:13 -0500

    Add middlewares passport, cors and express json

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,41 @@
-const express = require("express")
+import express from "express"
+import cors from  "cors"
+import authRoutes from "./routes/auth.route.js"
+import session from "express-session"
+import passport from "passport";
+import {dotenv_config} from "./config/dotenv.js";
+
+// const imagesRouters = require("./routes/images.routes")
+
+
 const app = express()
 
+const PORT = dotenv_config.port
+
+
+app.use(cors())
+app.use(express.json())
+
+
+app.use(session({
+    secret: "supersecret",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use("/api", authRoutes)
 
 app.get("/", (req,res) => {
-    res.send("hello! ")
+
 })
 
 
-app.listen(3500, () => {
-    console.log("----------------------- server runing -----------------------")
+
+
+app.listen(PORT, () => {
+    console.log(`----------------------- server runing on the port: ${PORT} -----------------------`)
 })
