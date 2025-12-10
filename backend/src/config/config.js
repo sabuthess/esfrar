f0b5cef9dc51fba7e951c518a9bb1fdba7fe09b4
sabuthess <dilan.developer22@gmail.com>
2025-12-09 19:37:08 -0500

    Chagen way in wich  packeges are imported

---
+++ backend/src/config/config.js
@@ -0,0 +1,26 @@
+import mysql from "mysql2/promise.js";
+import {dotenv_config} from "./dotenv.js";
+
+async function connectDB() {
+	const pool = mysql.createPool({
+		host: dotenv_config.db.host,
+		user: dotenv_config.db.user,
+		password: dotenv_config.db.password,
+		database: dotenv_config.db.name,
+		waitForConnections: true,
+		connectionLimit: 10,
+		connectTimeout: 10000,
+	});
+
+	try {
+		const connection = await pool.getConnection();
+		console.log("Connecting to  MySQL whit pool");
+		connection.release();
+	} catch (err) {
+		console.error("Error to the connecting MySQL:", err);
+	}
+
+	return pool;
+}
+
+export default connectDB;
---
+++ backend/src/config/dotenv.js
@@ -0,0 +1,16 @@
+import  dotenv from "dotenv";
+
+dotenv.config();
+
+export const dotenv_config = {
+	port: process.env.PORT || 8000,
+	db: {
+		host: process.env.DB_HOST,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		name: process.env.DB_NAME,
+	},
+	frontend_url_base: process.env.FRONTEND_URL_BASE,
+};
+
+
